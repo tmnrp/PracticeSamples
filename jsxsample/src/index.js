741908df6f9e5d92a4773b2616bb85d59da501ec
Harsh Parammal <tony.venom007@gmail.com>
2019-12-07 22:34:28 -0800

    Modified app.js + JSX sample changing value of button

--- jsxsample/src/index.js
+++ jsxsample/src/index.js
@@ -2,9 +2,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getText(str) {
+    return str;
+}
+
 // Create a react component
-const App = function() {
-    return <div> Hello World </div>
+const App = function () {
+    const buttonText = {text: 'Click me !'};
+    const styleObj = { backgroundColor: 'pink', color: 'white' };
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter Name:
+            </label>
+            <input id="name" type="text" />
+            <button style={styleObj}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
 }
 
 
