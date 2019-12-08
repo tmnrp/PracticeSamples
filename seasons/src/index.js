c08a39a7281f22fca03abebb52606b2658ac8f60
Harsh Parammal <tony.venom007@gmail.com>
2019-12-08 05:04:37 -0800

    class components, constructor, render function + geolocation async

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,23 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
+class App extends React.Component {
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-            console.log(position);
-        },
-        (errorMsg) => {
-            console.log(errorMsg);
-        }
-    );
+    constructor(props) {
+        super(props);
+        this.state = { latitude: 10 };
 
-    return (
-        <div >
-            Hello World
-        <SeasonDisplay />
-        </div>
-    );
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position);
+                this.setState({ latitude: position.coords.latitude });
+
+            },
+            (errorMsg) => {
+                console.log(errorMsg);
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div >
+                Hello World : you are in {this.state.latitude}.
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
