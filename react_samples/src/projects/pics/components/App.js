bdc3c4e56f95d0804336f3a29bc428d0b55ed2d7
Harsh Parammal <tony.venom007@gmail.com>
2019-12-13 17:05:55 +0530

    Usplash-axios-get-promise-update state with response.

--- react_samples/src/projects/pics/components/App.js
+++ react_samples/src/projects/pics/components/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className='ui container' style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+    state = { images: [] };
+
+    onSearchBarSubmit = (searchKey) => {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: searchKey
+            },
+            headers: {
+                Authorization: 'Client-ID 698b5409d7c806c6982ec216e65d068f5851b04d470fc6729457ae908b78bb18'
+            }
+        }).then((response) => {
+            var results = response.data.results;
+            this.setState({ images: results });
+            this.setState({ imagesLength: results.length });
+            console.log(results);
+        });
+    };
+
+    render() {
+        return (
+            <div className='ui container' style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchBarSubmit} />
+                <label >Found : {this.state.imagesLength} images.</label>
+            </div>
+        );
+    };
+}
 
 export default App;
--- react_samples/src/projects/pics/components/SearchBar.js
+++ react_samples/src/projects/pics/components/SearchBar.js
@@ -1,26 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { searchKey: '', isUpper: 'off' };
+    state = { searchKey: '' };
 
-    onInputTextChange(event) {
-        var searchKey = (this.state.isUpper ? event.target.value.toUpperCase() : event.target.value.toLowerCase());
-        this.setState({ searchKey: searchKey });
+    onInputTextChange = (event) => {
+        this.setState({ searchKey: event.target.value });
     }
 
-    onInputCheckBoxChange(event) {
-        this.setState({ isUpper: event.target.checked });
-        this.setState({searchKey: this.state.searchKey});
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.searchKey);
     }
 
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
                         <label >Image search :</label>
-                        <input type='checkbox' value={this.state.isUpper} onChange={this.onInputCheckBoxChange.bind(this)} />
-                        <input type='text' value={this.state.searchKey} onChange={this.onInputTextChange.bind(this)} />
+                        <input type='text' value={this.state.searchKey} onChange={this.onInputTextChange} />
                     </div>
                 </form>
             </div>
